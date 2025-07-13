60b81e634f7e9230ad270e52d96b28ed45e06877
zoldyck6666 <mehdi.bouzangad@gmail.com>
2025-07-13 15:58:21 +0100

    Add Header, MainContent, Footer components and update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
