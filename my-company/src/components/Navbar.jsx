f14cdb9f05ab54021052100a27cfb08d0e8f0bf7
zoldyck6666 <mehdi.bouzangad@gmail.com>
2025-07-20 23:28:44 +0100

    Fix Navbar inline styling: add justifyContent

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const navStyle = {
+  display: "flex",
+  justifyContent: "space-between",  // This is the required property
+  alignItems: "center",
+  padding: "10px 20px",
+  backgroundColor: "#333",
+  color: "white",
+};
+
+const linkStyle = {
+  color: "white",
+  textDecoration: "none",
+  margin: "0 10px",
+};
+
 function Navbar() {
-  const navStyle = {
-    display: "flex",
-    gap: "15px",
-    padding: "10px",
-    backgroundColor: "#333",
-    color: "#fff",
-  };
-
-  const linkStyle = {
-    color: "#fff",
-    textDecoration: "none",
-  };
-
   return (
     <nav style={navStyle}>
-      <Link to="/" style={linkStyle}>Home</Link>
-      <Link to="/about" style={linkStyle}>About</Link>
-      <Link to="/services" style={linkStyle}>Services</Link>
-      <Link to="/contact" style={linkStyle}>Contact</Link>
+      <div>
+        <Link to="/" style={linkStyle}>
+          Home
+        </Link>
+        <Link to="/about" style={linkStyle}>
+          About
+        </Link>
+        <Link to="/services" style={linkStyle}>
+          Services
+        </Link>
+        <Link to="/contact" style={linkStyle}>
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 }
