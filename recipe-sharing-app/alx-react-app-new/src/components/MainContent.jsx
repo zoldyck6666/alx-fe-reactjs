c476a85b3f1f53e761d5044d7a05a686db0e7e53
zoldyck6666 <mehdi.bouzangad@gmail.com>
2025-07-27 16:59:36 +0100

    Add recipe-sharing-app as normal folder

---
+++ recipe-sharing-app/alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', fontSize: '18px', color: 'darkslategray' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+
+function App() {
+  return (
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
