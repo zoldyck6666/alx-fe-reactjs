3cedc73bfd268c4d50f494920d1ce38edade12f7
zoldyck6666 <mehdi.bouzangad@gmail.com>
2025-07-27 16:34:33 +0100

    Delete src directory

--- src/recipeStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-
-export const useRecipeStore = create((set) => ({
-  recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
-}));
