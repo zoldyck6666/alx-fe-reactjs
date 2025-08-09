31d25121040c42e9db9b2bc817588afccfd8df6f
zoldyck6666 <mehdi.bouzangad@gmail.com>
2025-08-09 20:35:11 +0100

    feat: add Tailwind CSS setup in tailwind-react-integration

---
+++ tailwind-react-integration/alx-react-app-new/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+function UserProfile(props) {
+  return (
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '8px' }}>
+      <h2 style={{ color: 'blue' }}>{props.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
---
+++ tailwind-react-integration/recipe-sharing-app/alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#eee', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
