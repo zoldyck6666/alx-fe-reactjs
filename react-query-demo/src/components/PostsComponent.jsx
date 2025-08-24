80868f159945982afd9a39a702266a2c66e00ff1
zoldyck6666 <mehdi.bouzangad@gmail.com>
2025-08-24 13:39:12 +0100

    Complete React Query demo with PostsComponent

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,26 @@
+import { useQuery } from "react-query";
+
+const fetchPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return res.json();
+};
+
+const PostsComponent = () => {
+  const { data, error, isLoading, refetch } = useQuery("posts", fetchPosts);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error fetching posts</div>;
+
+  return (
+    <div>
+      <button onClick={() => refetch()}>Refetch Posts</button>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
