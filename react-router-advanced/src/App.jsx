3cb3e1d8eba5b08c37e615120e3f8be846da1850
zoldyck6666 <mehdi.bouzangad@gmail.com>
2025-08-24 14:05:25 +0100

    Add dynamic routes and protected /profile route in App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,11 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import BlogPost from "./components/BlogPost"; // import the component
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
+import Profile from './components/Profile';
+import ProfileDetails from './components/ProfileDetails';
+import ProfileSettings from './components/ProfileSettings';
+import BlogPost from './components/BlogPost';
+import Home from './components/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Other routes */}
+        {/* Home page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Protected /profile route with nested routes */}
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+
+        {/* Dynamic blog post route */}
         <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
